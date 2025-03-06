d4a01a511a14d2f208f3682aaf6399cfe35d852a
StivenDelgado <delgadostiven123@gmail.com>
2025-03-06 16:47:01 -0500

    upload portafolio

---
+++ README.md
@@ -0,0 +1,53 @@
+# Portfolio
+
+---
+
+[Demo](https://stivendelgado.vercel.app/)
+
+[Astro Themes](https://astro.build/themes/details/dark-minimal/)
+
+[ReactBits Showcase](https://www.reactbits.dev/showcase) 
+
+The component `<LetterGlitch \>` was taken from [ReactBits.dev](https://www.reactbits.dev/)
+
+## **Stack**  
+### **Frontend**  
+![Astro](https://img.shields.io/badge/Astro-FF5D01?logo=astro&logoColor=white)
+![Tailwind](https://img.shields.io/badge/Tailwind_CSS-38B2AC?logo=tailwind-css&logoColor=white)
+![TypeScript](https://img.shields.io/badge/TypeScript-3178C6?logo=typescript&logoColor=white)
+
+### **Tools**  
+![Figma](https://img.shields.io/badge/Figma-F24E1E?logo=figma&logoColor=white)
+![Prettier](https://img.shields.io/badge/Prettier-F7B93E?logo=prettier&logoColor=black)
+![Canva](https://img.shields.io/badge/Canva-c900c3?logo=canva&logoColor=white)
+
+
+## **Project structure**
+```
+public/
+└── svg/
+src/
+├── Components/
+├── layouts/
+├── React/
+└── pages/
+```
+
+## **Local configuration** 
+1. Clone the repo:  
+```
+git clone https://github.com/gothsec/portfolio
+```
+2. Install dependencies:
+```  
+npm install
+```
+3. Start the develpoment server:
+```  
+npm run dev
+```
+
+> **Important Notice:**  
+> This project is licensed under the [MIT License](https://opensource.org/licenses/mit).  
+> According to the license terms, any redistribution (including compiled or modified versions), you **must** retain the original copyright 
+> notice and the full license text. Copyright © 2025 Oscar Hernandez. All rights reserved.
---
+++ src/React/SkillsList.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+
+const CategoryIcons = {
+  "Web Development": (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6 text-[var(--sec)] opacity-70"
+    >
+      <path d="M21 3C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H21ZM20 11H4V19H20V11ZM20 5H4V9H20V5ZM11 6V8H9V6H11ZM7 6V8H5V6H7Z"></path>
+    </svg>
+  ),
+  "Database Management": (
+    <svg xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6 text-[var(--sec)] opacity-70">
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" />
+    </svg>
+  ),
+  "DevOps": (
+    <svg xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6 text-[var(--sec)] opacity-70">
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" />
+      <path d="M12 6l-5 5 5 5 5-5-5-5zm0 8.5l-3.5-3.5L12 7.5l3.5 3.5L12 14.5z" />
+    </svg>
+  ),
+};
+
+const SkillsList = () => {
+  const [openItem, setOpenItem] = useState<string | null>(null);
+
+  const skills = {
+    "Web Development": [
+      "Single Page Applications (SPAs)",
+      "Landing pages and business websites",
+      "Portfolio websites",
+    ],
+    "Database Management": [
+      "Relational databases (MySQL, PostgreSQL)",
+      "NoSQL databases (MongoDB, Firebase)"
+    ],
+    "DevOps": [
+      "CI/CD pipelines (GitHub Actions)",
+      "Containerization with Docker",
+      "Cloud platforms (AWS, Azure)"
+    ],
+  };
+
+  const toggleItem = (item: string) => {
+    setOpenItem(openItem === item ? null : item);
+  };
+
+  return (
+    <div className="text-left pt-3 md:pt-9">
+      <h3 className="text-[var(--white)] text-3xl md:text-4xl font-semibold md:mb-6">
+        What I do?
+      </h3>
+      <ul className="space-y-4 mt-4 text-lg">
+        {Object.entries(skills).map(([category, items]) => (
+          <li key={category} className="w-full">
+            <div
+              onClick={() => toggleItem(category)}
+              className="md:w-[400px] w-full bg-[#1414149c] rounded-2xl text-left hover:bg-opacity-80 transition-all border border-[var(--white-icon-tr)] cursor-pointer overflow-hidden"
+            >
+              <div className="flex items-center gap-3 p-4">
+                {CategoryIcons[category]}
+                <div className="flex items-center gap-2 flex-grow justify-between">
+                  <div className="min-w-0 max-w-[200px] md:max-w-none overflow-hidden">
+                    <span className="block truncate text-[var(--white)] text-lg">
+                      {category}
+                    </span>
+                  </div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className={`w-6 h-6 text-[var(--white)] transform transition-transform flex-shrink-0 ${openItem === category ? "rotate-180" : ""
+                      }`}
+                  >
+                    <path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div
+                className={`transition-all duration-300 px-4 ${openItem === category
+                  ? "max-h-[500px] pb-4 opacity-100"
+                  : "max-h-0 opacity-0"
+                  }`}
+              >
+                <ul className="space-y-2 text-[var(--white-icon)] text-sm">
+                  {items.map((item, index) => (
+                    <div key={index} className="flex items-center">
+                      <span className="pl-1">•</span>
+                      <li className="pl-3">{item}</li>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SkillsList;
